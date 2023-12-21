f6ba03f2bd649cbb12de92b7cc664abd8646932b
WebLab04 <ivanlabunets209@gmail.com>
2023-12-21 09:49:50 +0100

    new

--- script.js
+++ script.js
@@ -9,39 +9,40 @@ else{
     let message;
     const currentYear = new Date().getFullYear();
     const currentAge = currentYear - userBirthAge;
-    
     message = `Ваш рік становить ${currentAge}\n`;
 
-    if(userLocation.toLowerCase() === 'київ' || userLocation.toLowerCase() === 'лондон' || userLocation.toLowerCase() === 'вашингтон'){
-       let country;
-        switch(userLocation.toLowerCase()){
-            case 'київ':
-                country = 'Україна';
-                break;
-            case 'вашингтон':
-                country = 'CША';
-                break;
-            case 'лондон':
-                country = 'Великої Британії';
-                break;  
-            default:
-                country = 'інакшої країни';  
-        }
-        message += `Ти живеш у столиці ${country}`;
-    }
-    else{
-        message += `Ти живеш у місті ${userLocation}`;
+    
+    let country;
+    const capitalCityAnswer = `Ти живеш у столиці ${country}`;
+    const anotherCityAnswer = `Ти живеш у місті ${userLocation}`;
+    const userLocationLowerCase = userLocation.toLocaleLowerCase();
+    switch(userLocationLowerCase){
+        case 'київ':
+            country = 'Україна';
+            message += capitalCityAnswer;
+            break;
+        case 'вашингтон':
+            country = 'CША';
+            message += capitalCityAnswer;
+            break;
+        case 'лондон':
+            country = 'Великої Британії';
+            message += capitalCityAnswer;
+            break;  
+        default:
+            message += anotherCityAnswer; 
     }
     alert(message);
 }
 
+const userPreferSportLowerCase = userPreferSport.toLowerCase();
 const sportChampions = {
     'футбол':'Андрій Шевченко',
     'спортивна акробатика': 'Микола Щербак та Сергій Попов',
     'баскетбол':'Леброн Джеймс'
 };
-if(userPreferSport.toLowerCase() in sportChampions){
-    const champion = sportChampions[userPreferSport.toLocaleLowerCase()];
+if(userPreferSportLowerCase in sportChampions){
+    const champion = sportChampions[userPreferSportLowerCase];
     const sportMessage = `Круто! Хочеш стати як ${champion}?"`;
     alert(sportMessage);
 }
